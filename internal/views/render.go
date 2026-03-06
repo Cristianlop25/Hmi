2f06ed17f11f1728d817eacd353d63351585e814
Cristian Lopez <cristiandavid.lopez.ext@circontrol.com>
2026-03-06 16:49:35 +0100

    Connector template generated

--- internal/views/render.go
+++ internal/views/render.go
@@ -6,19 +6,17 @@ import (
 )
 
 func Render(w http.ResponseWriter, r *http.Request, name string, data any) {
-	if r.Header.Get("HX-Request") == "true" {
-		tpl := template.Must(template.ParseFiles(
-			"templates/pages/" + name + ".html",
-		))
-
-		_ = tpl.ExecuteTemplate(w, "content", data)
-		return
-	}
-
 	tpl := template.Must(template.ParseFiles(
 		"templates/layouts/base.html",
 		"templates/pages/"+name+".html",
 	))
+
+	tpl = template.Must(tpl.ParseGlob("templates/components/*.html"))
+
+	if r.Header.Get("HX-Request") == "true" {
+		_ = tpl.ExecuteTemplate(w, "content", data)
+		return
+	}
 
 	_ = tpl.ExecuteTemplate(w, "base", data)
 }
